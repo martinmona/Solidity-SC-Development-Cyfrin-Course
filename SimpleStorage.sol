2c9bdc3ed69906a47b17a4669ea75c998be2fec7
martinmona <martin.m94@hotmail.com>
2025-12-18 11:40:21 -0300

    Add list of people

--- SimpleStorage.sol
+++ SimpleStorage.sol
@@ -2,13 +2,28 @@
 pragma solidity 0.8.19;
 
 contract SimpleStorage {
-    uint256 public favoriteNumber;
+    uint256 myFavoriteNumber;
+
+    // uint256[] listOfFavoriteNumbers;
+
+    struct Person {
+        string name;
+        uint256 favoriteNumber;
+    }
+
+    Person[] public listOfPeople;
+
+    Person public pochi = Person({favoriteNumber: 7, name: "pochi"});
 
     function store(uint256 _favoriteNumber) public {
-        favoriteNumber = _favoriteNumber;
+        myFavoriteNumber = _favoriteNumber;
     }
 
     function retrieve() public view returns(uint256) {
-        return favoriteNumber;
+        return myFavoriteNumber;
+    }
+
+    function addPerson(string memory _name, uint256 _favoriteNumber) public {
+        listOfPeople.push(Person(_name, _favoriteNumber));
     }
 }
