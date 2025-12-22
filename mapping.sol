2e758a8745624a9bd682e976e9f3bf5042c37326
martinmona <martin.m94@hotmail.com>
2025-12-22 12:17:12 -0300

    add _exists mapping

--- mapping.sol
+++ mapping.sol
@@ -3,21 +3,30 @@
 pragma solidity 0.8.19;
 
 contract Mapping {
-    mapping(address => uint256) balances;
+    address[] accounts;
+    mapping(address => uint256) private _balances;
+    mapping(address => bool) private _exists;
+
+    function createAccount(address _newAddress) public {
+        accounts.push(_newAddress);
+        _exists[_newAddress] = true;
+    }
 
     function deposit(address _myAddress, uint256 _deposit) public returns(uint256) {
-        balances[_myAddress] += _deposit;
-        return balances[_myAddress];
+        _balances[_myAddress] += _deposit;
+        require(_exists[_myAddress], 'Account does not exists');
+        return _balances[_myAddress];
     }
 
     function withdraw(address _myAddress, uint256 amount) public returns(uint256) {
-        if (balances[_myAddress] - amount < 0)
-            revert('Not enought founds');
-        balances[_myAddress] -= amount;
-        return balances[_myAddress];
+        require(_exists[_myAddress], 'Account does not exists');
+        require (_balances[_myAddress] - amount < 0, 'Not enough founds');
+        _balances[_myAddress] -= amount;
+        return _balances[_myAddress];
     }
 
     function checkBalance(address _myAddress) public view returns(uint256){
-        return balances[_myAddress];
+        require(_exists[_myAddress], 'Account does not exists');
+        return _balances[_myAddress];
     }
 }
