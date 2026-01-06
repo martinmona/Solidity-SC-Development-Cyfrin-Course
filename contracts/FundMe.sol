01cc8cd0c255e706b084af874619930e871cd89e
martinmona <martin.m94@hotmail.com>
2026-01-06 11:20:49 -0300

    make found fn payable with require

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -3,7 +3,9 @@
 pragma solidity ^0.8.30;
 
 contract FundMe {
-    function found() public {}
+    function found() public payable {
+        require(msg.value > 1e18, "didn't send enough ETH");
+    }
 
     function widthraw() public {}
 }
