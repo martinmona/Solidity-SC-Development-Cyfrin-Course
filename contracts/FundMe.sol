b0acacfe75bc574301cd884e8442ff06f41c0ea3
martinmona <martin.m94@hotmail.com>
2026-01-09 11:17:07 -0300

    create MathLibrary and sum function

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -3,8 +3,10 @@
 pragma solidity ^0.8.30;
 
 import {PriceConverter} from "./PriceConverter.sol";
+import "./MathLibrary.sol";
 
 contract FundMe {
+    using MathLibrary for uint256;
     using PriceConverter for uint256;
     uint256 public minimumUsd = 5e18;
     address[] public funders;
@@ -13,7 +15,7 @@ contract FundMe {
     function found() public payable {
         require(msg.value.getConversionRate() > minimumUsd, "didn't send enough ETH");
         funders.push(msg.sender);
-        addressToAmountFunded[msg.sender] += msg.value;
+        addressToAmountFunded[msg.sender] = msg.value.sum(addressToAmountFunded[msg.sender]);
     }
 
     function widthraw() public {}
