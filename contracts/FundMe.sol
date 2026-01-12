56097b4997e439767697d3ee9794a7a657d37065
martinmona <martin.m94@hotmail.com>
2026-01-12 12:48:18 -0300

    Use immutable, constant and custom error to save gas

--- contracts/FundMe.sol
+++ contracts/FundMe.sol
@@ -3,22 +3,29 @@
 pragma solidity ^0.8.30;
 
 import {PriceConverter} from "./PriceConverter.sol";
-import "./MathLibrary.sol";
+import {MathLibrary} from "./MathLibrary.sol";
+
+error NotOwner();
 
 contract FundMe {
     using MathLibrary for uint256;
     using PriceConverter for uint256;
-    uint256 public minimumUsd = 5e18;
+    uint256 public constant MINIMUM_USD = 5e18;
     address[] public funders;
     mapping(address funder => uint256 amountFunded) public addressToAmountFunded;
+
+    address public immutable i_owner;
+    constructor() {
+        i_owner = msg.sender;
+    }
     
     function found() public payable {
-        require(msg.value.getConversionRate() > minimumUsd, "didn't send enough ETH");
+        require(msg.value.getConversionRate() > MINIMUM_USD, "didn't send enough ETH");
         funders.push(msg.sender);
         addressToAmountFunded[msg.sender] = msg.value.sum(addressToAmountFunded[msg.sender]);
     }
 
-    function widthrawAll() public {
+    function widthrawAll() onlyOwner public {
         for(uint256 i = 0; i < funders.length; i++) {
             address funder = funders[i];
             addressToAmountFunded[funder] = 0;
@@ -36,5 +43,13 @@ contract FundMe {
         (bool success,) = payable(msg.sender).call{value: address(this).balance}("");
         require(success, "call failed");
 
+    }
+
+    modifier onlyOwner() {
+        // require(msg.sender == i_owner ,"Owner is not the sender");
+        if(msg.sender != i_owner) {
+            revert NotOwner();
+        }
+        _;
     }
 }
